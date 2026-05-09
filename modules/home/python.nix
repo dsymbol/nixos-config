2ce6b9a5075fa513c3e1bbfbce990c1f998e2147
dsymbol <dsymbol@users.noreply.github.com>
2026-05-09 21:17:13 +0300

    add uv settings

--- modules/home/python.nix
+++ modules/home/python.nix
@@ -6,8 +6,14 @@
 {
   home.packages = with pkgs; [
     (python3.withPackages (ps: with ps; [ tkinter ]))
-    uv
     ty
     ruff
   ];
+
+  programs.uv = {
+    enable = true;
+    settings = { 
+      exclude-newer = "7 days";
+    };
+  };
 }
