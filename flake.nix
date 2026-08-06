7f19247ce1d4076173b7a9d4fafc92002ab6ae03
dsymbol <dsymbol@users.noreply.github.com>
2026-08-06 16:25:01 +0300

    clean flake

--- flake.nix
+++ flake.nix
@@ -6,8 +6,10 @@
 
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-    home-manager.url = "github:nix-community/home-manager/release-26.05";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-26.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     plasma-manager = {
       url = "github:nix-community/plasma-manager";
@@ -21,53 +23,30 @@
     };
   };
 
-  outputs =
-    {
-      self,
-      nixpkgs,
-      nixpkgs-unstable,
-      ...
-    }@inputs:
+  outputs = { nixpkgs, nixpkgs-unstable, ... }@inputs:
     let
       username = "user";
 
-      mkSystem =
-        { host, system }:
-        let
-          pkgs-unstable = import nixpkgs-unstable {
-            inherit system;
-            config = {
-              allowUnfree = true;
-            };
-          };
-        in
+      mkSystem = host: system:
         nixpkgs.lib.nixosSystem {
           inherit system;
-          specialArgs = {
-            inherit
-              inputs
-              username
-              host
-              pkgs-unstable
-              ;
-          };
           modules = [
             {
               nixpkgs.config.allowUnfree = true;
             }
             ./hosts/${host}/configuration.nix
           ];
+          specialArgs = {
+            inherit inputs username host;
+            pkgs-unstable = import nixpkgs-unstable {
+              inherit system;
+              config.allowUnfree = true;
+            };
+          };
         };
     in
     {
-      nixosConfigurations.desktop = mkSystem {
-        host = "desktop";
-        system = "x86_64-linux";
-      };
-
-      nixosConfigurations.virtualbox = mkSystem {
-        host = "virtualbox";
-        system = "x86_64-linux";
-      };
+      nixosConfigurations.desktop = mkSystem "desktop" "x86_64-linux";
+      nixosConfigurations.virtualbox = mkSystem "virtualbox" "x86_64-linux";
     };
 }
